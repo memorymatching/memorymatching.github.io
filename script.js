def0b3bd2a01f3b40d1cc055f16340837a3bab92
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-21 19:12:09 -0500

    Moved instructions and game screens to JS; formatted playing instructions in readme

---
+++ script.js
@@ -0,0 +1,23 @@
+/*** GAME ***/
+/*
+<section class="game">
+<p class="time-left">Time Left</p>
+<p class="pairs-found">Pairs Found</p>
+<button class="play restart">Restart</button>
+<div class="board"></div>
+</section>
+*/
+
+/*** INSTRUCTIONS ***/
+/*
+<img class="title" src="assets/img/chemInTheHouse/titleBanner.png" alt="Chem in the House">
+<section class="instructions two-column">
+    <div class="text btns">
+        <h3>How to Play</h3>
+        <p class="center">The goal is to match all 8 pairs of cards within 3 minutes. To begin, click on a card to flip it over, and try to find its matching pair by clicking on another card. Do your best to rememember the locations of the mismatched pairs as you continue flipping cards. If you find all the pairs before the time runs out, you win! If 3 minutes pass before you match all the pairs, the cards will shuffle, the timer will reset, and you will get another chance.</p>
+        <p class="center"><i>Good luck!</i></p>
+        <button class="play">Play</button>
+    </div>
+    <img src="assets/img/chemInTheHouse/logoTransparent.png" alt="Chem in the House logo">
+</section>
+*/
